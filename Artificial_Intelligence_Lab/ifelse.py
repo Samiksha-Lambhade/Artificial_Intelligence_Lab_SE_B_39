5c70b29a398d25effad46304d37d84853930f581
gauriwagare <prem.ghodke24@mmit.edu.in>
2025-07-23 11:53:43 +0530

    file is generated

---
+++ Artificial_Intelligence_Lab/ifelse.py
@@ -0,0 +1,23 @@
+subject1 =str(input("Enter the subject1="))
+subject2 =str(input("Enter the subject2="))
+
+if(subject1=="Maths" and subject2=="Physics"):
+            print("Mechanical Engineering")
+            
+elif(subject1=="Programming" and subject2=="Maths"): 
+            print("Computer Engineering")
+            
+elif(subject1=="Biology" and subject2=="Chemistry"): 
+            print("Biotechnology")
+            
+elif(subject1=="Circuits" and subject2=="Maths"):  
+            print("Electronics Engineering")
+            
+elif(subject1=="Programming" and subject2=="Statistics"): 
+            print("Artificial Intelligence and Data Science")
+            
+elif(subject1=="Programming" and subject2=="AI Concepts"):
+            print("Artificial Intelligence and Machine Learning Engineering")
+else:
+            print("Invalid")
+            
